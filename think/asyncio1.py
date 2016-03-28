a72390eee6090b170716a7ca12924f6a620d6bf1
dujiacheng <du@fireroad.local>
2016-03-28 18:54:17 +0800

    greate orm

---
+++ think/asyncio1.py
@@ -0,0 +1,13 @@
+import asyncio
+
+@asyncio.coroutine
+def hello():
+    print("Hello world!")
+    r = yield from asyncio.sleep(2)
+    print("Hello again!")
+
+
+loop = asyncio.get_event_loop()
+
+loop.run_until_complete(hello())
+loop.close()
